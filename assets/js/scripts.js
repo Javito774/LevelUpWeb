5cbb227cceb32973bb2afebe5844fc216377e309
javierherrerosan <javierherrerosan@alumnos.upm.es>
2022-03-24 20:36:39 +0100

    Busqueda de errores en cookies

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -15,15 +15,16 @@
         let ca = decodedCookie.split(';');
         for(let i = 0; i <ca.length; i++) {
             let c = ca[i];
-            while (c.charAt(0) == ' ') {
+            while (c.charAt(0) === ' ') {
                 c = c.substring(1);
             }
-            if (c.indexOf(name) == 0) {
+            if (c.indexOf(name) === 0) {
                 return c.substring(name.length, c.length);
             }
         }
         return undefined;
     }
+    console.log(getCookie("AceptarCookies"));
     if (getCookie("AceptarCookies") !== undefined) {
         mostrarCookies();
     }
